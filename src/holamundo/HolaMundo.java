a1037ec592b6ada91f46ec833639144fc4587725
Israel <israelmaganasolis@gmail.com>
2022-02-17 09:33:54 -0700

    Agregamos un nuevo mensaje

--- src/holamundo/HolaMundo.java
+++ src/holamundo/HolaMundo.java
@@ -16,6 +16,7 @@ public class HolaMundo {
      */
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        System.out.println("Esta es la primera modificacion");
     }
     
 }
